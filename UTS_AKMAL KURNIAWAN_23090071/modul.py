306d6bc37d6386923c6c93461b7958f31fd37368
AkmalKurniawan007 <148427738+AkmalKurniawan007@users.noreply.github.com>
2024-05-07 17:10:53 +0700

    Add files via upload

---
+++ UTS_AKMAL KURNIAWAN_23090071/modul.py
@@ -0,0 +1,27 @@
+def kal_bmi():
+    def hitung_bmi(berat_badan, tinggi_badan):
+        bmi = berat_badan / (tinggi_badan ** 2)
+        return bmi
+
+    def berikan_rekomendasi(bmi):
+        if bmi < 18.5:
+            return " Berat badan kurang. Makan yang berprotein!!! ."
+        elif 18.5 <= bmi < 24.9:
+            return "Berat badan normal. Sip Bro pertahankan!!!! ."
+        elif 25 <= bmi < 29.9:
+            return "Kelebihan berat badan. Jangan kebanyakan Makan dong dan Olahraga juga JANGAN MALAS....!!!!!."
+        elif bmi >= 30 :
+            return "obesitas. Disarankan untuk berkonsultasi dengan dokter untuk mendapatkan nasihat medis."
+        else:
+            return "Anda Sepertinya Bukan Orang"
+
+            
+
+    berat_badan = float(input("Masukkan berat badan (Kg): "))
+    tinggi_badan = float(input("Masukkan tinggi badan (meter): "))
+
+    bmi = hitung_bmi(berat_badan, tinggi_badan)
+    rekomendasi = berikan_rekomendasi(bmi)
+
+    print("BMI Anda:", bmi)
+    print("Rekomendasi Kesehatan:", rekomendasi)
